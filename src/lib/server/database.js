e7ddcc1ce0904686768378db8ddde0116fcc172d
Kevin Tran <143236084+Anonymasuru@users.noreply.github.com>
2024-10-22 13:12:10 -0700

    further modifications to database

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,10 +2,10 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-// import {PGCONNECT} from '$env/static/private';
+//import {PGCONNECT} from '$env/static/private';
 const pgConnect = process.env.PGCONNECT;
 
 
-const sql = postgres(PGCONNECT, {} )
+const sql = postgres(pgConnect, {} )
 
 export default sql;
