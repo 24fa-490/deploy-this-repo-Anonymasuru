efef3483420ea2234c8d004f64c018c0c6203d44
Kevin Tran <143236084+Anonymasuru@users.noreply.github.com>
2024-10-22 12:26:10 -0700

    .env attempt 2

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,8 +2,8 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-// import {PGCONNECT} from '$env/static/private';
-import {PGCONNECT} from '$env';
+import {PGCONNECT} from '$env/static/private';
+// import {PGCONNECT} from '$env';
 
 
 const sql = postgres(PGCONNECT, {} )
